5f0f0311c396b278857b0d7ad9c53ab2f2ee1e50
Katherine Rosenkranz <katherine.rosenkranz@florencehc.com>
2018-06-17 19:34:01 -0400

    adding auth services

--- src/app/auth/roleguard.service.ts
+++ src/app/auth/roleguard.service.ts
@@ -1,44 +1,17 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
-import { Router } from '@angular/router';
-import { tokenNotExpired } from 'angular2-jwt';
-import { Observable } from 'rxjs';
-import { API_URL } from './../app.constants';
-import * as jwtDecode from 'jwt-decode';
+import { Router, CanActivate } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Injectable()
-export class RoleGuard {
+export class RoleGuard implements CanActivate {
 
-  constructor(private http: Http, private router: Router) { }
+  constructor(private auth: AuthService, private router: Router) { }
 
-  getToken(): any {
-    return localStorage.getItem('token');
+  canActivate(){
+    if(this.auth.isAuthenticated() && this.auth.isAdmin()){
+      return true;
+    } else {
+      this.router.navigate(['login']);
+    }
   }
-
-  getUseRole(): string {
-    return jwtDecode(this.getToken()).scope;
-  }
-
-  isAuthenticated(): boolean {
-    return tokenNotExpired('token');
-  }
-
-  login(credentials): Observable<Response> {
-    return this.http.post(`${API_URL}/users/authenticate`, credentials);
-  }
-
-  signup(credentials): Observable<Response> {
-    return this.http.post(`${API_URL}/users`, credentials);
-  }
-
-  finishAuthentication(token): void {
-    localStorage.setItem('token', token)
-    this.router.navigate(['profile']);
-  }
-
-  logout(): void {
-    localStorage.removeItem('token');
-    this.router.navigate(['login']);
-  }
-
 }
