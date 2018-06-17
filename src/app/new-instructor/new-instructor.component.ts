9376d5a74167092e070bc0e2346aba57998b922b
Katherine Rosenkranz <katherine.rosenkranz@florencehc.com>
2018-06-17 19:58:03 -0400

    adding new instructor component

--- src/app/new-instructor/new-instructor.component.ts
+++ src/app/new-instructor/new-instructor.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { InstructorService } from '../instructor/instructor.service';
+import { Instructor } from '../instructor/instructor';
+import 'rxjs/add/opperator/map';
+
 
 @Component({
-  selector: 'app-new-instructor',
-  templateUrl: './new-instructor.component.html',
-  styleUrls: ['./new-instructor.component.css']
+    selector: 'app-new-instructor',
+    templateUrl: './new-instructor.component.html',
+    styleUrls: ['./new-instructor.component.css']
 })
 export class NewInstructorComponent implements OnInit {
+    instructor: Instructor;
 
-  constructor() { }
+    constructor(private instService: InstructorService, private router: Router, private location: Location) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    addInstructor(data){
+        this.instService.addInstructor(data)
+            .map(response => response.json())
+            .subscribe(data => this.router.navigate(['instructor']),
+                error => console.log(error));
+    }
+
+    goBack(){
+        this.location.back();
+    }
 
 }
